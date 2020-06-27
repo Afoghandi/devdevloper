384361c2a4ca66eddfc8bae25950b3f0f78f00a6
Afo Oshun <afoyenyen@Afos-iMac.local>
2020-06-27 02:23:31 +0100

    auth reducer and action

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,4 +1,6 @@
 import { combineReducers } from 'redux';
 
 import alert from './alert.js';
-export default combineReducers({ alert });
+import auth from './auth';
+
+export default combineReducers({ alert, auth });
